a32c2c676bd88c3b77d2b5d4214279e53fa4ae26
Moiz1495 <moizakram@hotmail.co.uk>
2015-09-03 18:15:45 +0500

    Create Calculator.java

---
+++ Lab-Task2/Calculator.java
@@ -0,0 +1,30 @@
+
+public class Calculator
+{
+	private int sum;
+	//public int num1, num2;
+	
+	public int add(int num1, int num2)
+	{
+		sum= num1+num2;
+		return sum;
+	}
+	
+	public int subtract(int num1, int num2)
+	{
+		sum= num1-num2;
+		return sum;
+	}
+	
+	public int multiply(int num1, int num2)
+	{
+		sum= num1*num2;
+		return sum;
+	}
+	
+	public int divide(int num1, int num2)
+	{
+		sum= num1/num2;
+		return sum;
+	}
+}
